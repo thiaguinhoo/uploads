496c696f4f3d3883fe1caf6f21432176e3b5b1b4
thiaguinhoo <thiagoluizsog@gmail.com>
2021-09-30 07:58:01 -0300

    added mlter config and uploads folder

--- src/application.js
+++ src/application.js
@@ -2,8 +2,12 @@ const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
+const multer = require('multer');
+
+const multerConfig = require('./config/multer');
 
 const application = express();
+const upload = multer(multerConfig);
 
 application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
@@ -14,5 +18,13 @@ if (process.env.NODE_ENV == 'development') {
     const morgan = require('morgan')
     application.use(morgan('tiny'));
 }
+
+application.post('/upload', upload.single('file'), async (request, response) => {
+    console.log('file', request.file);
+    if (!!request.file) {
+        return response.json({ filename: request.file.filename });
+    }
+    response.json({ error: true });
+});
 
 module.exports = application;
---
+++ src/config/multer.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const multer = require('multer');
+
+const fileFilter = (req, file, cb) => {
+    cb(null, true)
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, '../../uploads/'));
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+        cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+    }
+})
+
+module.exports = { storage };
